5093c753978d2ee363a28a2931b5b39b284aa19d
Ekaterina <Ekaterina.Paniukova@urfu.me>
2022-12-19 21:36:02 +0500

    добавлена связь с сервером

--- js/util.js
+++ js/util.js
@@ -1,17 +1,3 @@
-function getRandomNumber (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}//источник:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-
-function checkLengthString (line, maximum) {
-  return line.length <= maximum;
-}
-
-const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
-
 const isEscape = (evt) => evt.key === 'Escape';
 
-checkLengthString('14', 2222);
-export {getRandomNumber, getRandomElement, isEscape};
+export {isEscape};
